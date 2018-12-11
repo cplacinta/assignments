42ed3edf227cb2a4636b3ca1976154a7556cb178
cplacinta <kristian.placinta@gmail.com>
2018-12-11 14:22:00 +0200

    created 2 arrays, and implemented fight method in the main

--- GameMain.java
+++ GameMain.java
@@ -1,15 +1,30 @@
 package HomeWork.assignments;
 
 
+import static HomeWork.assignments.Personaj.println;
 
 public class GameMain {
     public static void main (String[] args){
-        Personaj [] rpg = new Personaj[1];
-        for(int i=0; i< rpg.length;i++){
-            rpg[i].randomize();
-            rpg[i].afisareaMsj();
+        Personaj [] frostwolves = new Personaj[10];
+        for(int i=0; i< frostwolves.length;i++){
+            frostwolves[i] = new Personaj();
+            frostwolves[i].randomize();
+            frostwolves[i].afisareaMsj();
         }
 
+        Personaj [] thunderLord = new Personaj[10];
+        for(int i=0; i< frostwolves.length;i++){
+            thunderLord[i] = new Personaj();
+            thunderLord[i].randomize();
+            println((thunderLord[i].afisareaMsj()));
+
+        }
+        // de creat 2 array din clanuri diferite care se vor lupta intre ei
+        for (int i =0; i<10; i++){
+            frostwolves[i].afisareDate();
+            thunderLord[i].afisareDate();
+            Personaj.fight(frostwolves[i],thunderLord[i]);
+        }
 
 
     }
