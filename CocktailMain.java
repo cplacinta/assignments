bf0a0e317b7377b701e198be41a72162a3e379fc
cplacinta <kristian.placinta@gmail.com>
2018-12-13 14:35:22 +0200

    added the implementation for static method

--- CocktailMain.java
+++ CocktailMain.java
@@ -2,7 +2,7 @@ package HomeWork.assignments;
 
 import java.util.*;
 
-public  class CocktailMain {
+public class CocktailMain {
     static void println(String str) {
         System.out.println(str);
     }
@@ -22,7 +22,7 @@ public  class CocktailMain {
         /*bar[0] = new Cocktail(); // apelare constructor implicit
         bar[1] = new Cocktail(); // apelare constructor implicit
         bar[2] = new Cocktail(); // apelare constructor implicit*/
-        bar[0] = new Cocktail("Martini",2,150); // apelare constructor cu parametri
+        /*bar[0] = new Cocktail("Martini",2,150); // apelare constructor cu parametri
         bar[1] = new Cocktail("Bloody Mary",2,250); // apelare constructor cu parametri
         bar[2] = new Cocktail("Long Beach",3,50); // apelare constructor cu parametri
 
@@ -33,55 +33,55 @@ public  class CocktailMain {
                 bar[i].gradAlcool[j] = sc.nextInt();
                 bar[i].pretIngrediente[j] = sc.nextFloat();
             }
-        }
+        }*/
 
         //Cocktail martini = new Cocktail(bar[0]); // apelare constructor de copiere
-        /*for (int i=0; i < nrCocktails; i++)
-            bar[i].randomize();*/
-
-
-        for (int i = 0; i < nrCocktails; i++)
+        for (int i = 0; i < nrCocktails; i++) {
+            bar[i] = new Cocktail();
+            bar[i].randomize();
             bar[i].afisare();
+        }
 
-        int maxIngrediente=0;
-        int indexIngrediente=0;
-        for(int i = 0; i<nrCocktails;i++)
-            if(maxIngrediente < bar[i].nrIngrediente) {
+
+        int maxIngrediente = 0;
+        int indexIngrediente = 0;
+        for (int i = 0; i < nrCocktails; i++)
+            if (maxIngrediente < bar[i].nrIngrediente) {
                 maxIngrediente = bar[i].nrIngrediente;
-                indexIngrediente=i;
+                indexIngrediente = i;
             }
-        println("Cea mai complicata bautura este: "+bar[indexIngrediente].nume + " care are " + bar[indexIngrediente].nrIngrediente + " ingrediente");
+        println("Cea mai complicata bautura este: " + bar[indexIngrediente].nume + " care are " + bar[indexIngrediente].nrIngrediente + " ingrediente");
 
-        println("Lista cocktail-uluri:");
-        for(int i=0; i< nrCocktails;i++){
-            println(bar[i].nume+" ");
+        println("Lista celor " + Cocktail.nrCocktails + " cocktail-uri este:");
+        for (int i = 0; i < nrCocktails; i++) {
+            println(bar[i].nume + " ");
 
         }
-        double avgGradAlcool=0;
-        int totalNrIngrediente =0;
-        for(int i = 0; i< nrCocktails;i++){
-            avgGradAlcool+=bar[i].sumGradAlc();
-            totalNrIngrediente+=bar[i].nrIngrediente;
+        double avgGradAlcool = 0;
+        int totalNrIngrediente = 0;
+        for (int i = 0; i < nrCocktails; i++) {
+            avgGradAlcool += bar[i].sumGradAlc();
+            totalNrIngrediente += bar[i].nrIngrediente;
         }
-        println("Gradul de alcool al tuturor bauturilor este de: " + (avgGradAlcool/totalNrIngrediente));
+        println("Gradul mediu de alcool al tuturor bauturilor este de: " + (avgGradAlcool / totalNrIngrediente));
 
-        double avgPriceIngredient=0;
-        for(int i = 0; i< nrCocktails;i++){
-            avgPriceIngredient+=bar[i].sumPrice();
+        double avgPriceIngredient = 0;
+        for (int i = 0; i < nrCocktails; i++) {
+            avgPriceIngredient += bar[i].sumPrice();
         }
-        println("Media ingredientilor este: " + (avgPriceIngredient/totalNrIngrediente));
+        println("Pretul mediu al tuturor ingredientilor este de: " + (avgPriceIngredient / totalNrIngrediente));
 
-        int maxPriceIngredient = 0;
-        for (int i=0; i<nrCocktails;i++)
-            if(maxPriceIngredient<bar[i].maxPrice())
-                maxIngrediente = bar[i].maxPrice();
-        println("Ingredientul cel mai costisitor este: " + maxIngrediente);
+        float maxPriceIngredient = 0;
+        for (int i = 0; i < nrCocktails; i++)
+            if (maxPriceIngredient < bar[i].maxPrice())
+                maxPriceIngredient = bar[i].maxPrice();
+        println("Ingredientul cel mai costisitor este: " + maxPriceIngredient);
 
         int maxgradAlcool = 0;
-        for (int i=0; i<nrCocktails;i++)
-            if(maxgradAlcool<bar[i].maxAlcool())
+        for (int i = 0; i < nrCocktails; i++)
+            if (maxgradAlcool < bar[i].maxAlcool())
                 maxgradAlcool = bar[i].maxAlcool();
-        println("Ingredientul cel mai costisitor este: " + maxgradAlcool);
+        println("Ingredientul cel mai mare grad alco este: " + maxgradAlcool);
 
 
     }
